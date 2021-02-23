fba5f227749fe34d246db958a845333b08617430
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-02-23 13:29:42 -0500

    updated time

--- src/java.js
+++ src/java.js
@@ -1,39 +1,6 @@
-function displayTemperature(response){
-console.log(response.data);
-let temperatureElement = document.querySelector("#now-temp");
-let cityElement = document.querySelector("#searched-city");
-let weatherDescriptionElement = document.querySelector("#now-weather-description")
-let humidityElement = document.querySelector("#now-humidity");
-let windSpeedElement = document.querySelector("#now-wind-speed");
-temperatureElement.innerHTML = Math.round(response.data.main.temp);
-cityElement.innerHTML = response.data.name;
-humidityElement.innerHTML = response.data.main.humidity;
-windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
-weatherDescriptionElement.innerHTML = response.data.weather[0].description;
-}
-
-let apiKey = "ed8ab9018735ed237ff0af3c6f9509f3"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Laval&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let now = new Date();
-function formatDate(date) {
-  let days = [
+function formatDate (timestamp){
+let date = new Date(timestamp);
+ let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -42,43 +9,39 @@ function formatDate(date) {
     "Friday",
     "Saturday"
   ];
-
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-
-  let currentYear = date.getFullYear();
-  let currentDay = days[date.getDay()];
-  let currentMonth = months[date.getMonth()];
-  let currentDate = date.getDate();
-
-  let formattedDate = `${currentDay}, ${currentMonth} ${currentDate}, ${currentYear}`;
-  return formattedDate;
+let currentDay = days[date.getDay()];
+let hours = date.getHours();
+if (hours <10){
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes <10){
+  minutes = `0${minutes}`;
+}
+return `${currentDay} ${hours}:${minutes}`;
 }
 
-let todaysDate = document.querySelector("#current-date");
-todaysDate.innerHTML = formatDate(now);
 
-function formatTime(time) {
-  let currentHour = time.getHours();
-  let currentMinute = time.getMinutes();
 
-  let formattedTime = `${currentHour}:${currentMinute}`;
-  return formattedTime;
+function displayTemperature(response){
+console.log(response.data);
+let temperatureElement = document.querySelector("#now-temp");
+let cityElement = document.querySelector("#searched-city");
+let weatherDescriptionElement = document.querySelector("#now-weather-description")
+let humidityElement = document.querySelector("#now-humidity");
+let windSpeedElement = document.querySelector("#now-wind-speed");
+let dateElement = document.querySelector("#current-date");
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+humidityElement.innerHTML = response.data.main.humidity;
+windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+weatherDescriptionElement.innerHTML = response.data.weather[0].description;
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let todaysTime = document.querySelector("#current-time");
-todaysTime.innerHTML = formatTime(now);
+let apiKey = "ed8ab9018735ed237ff0af3c6f9509f3"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Laval&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
 
 
