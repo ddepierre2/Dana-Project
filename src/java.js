d9fbcac0cdcd1b773b1ff87748571915c26e1143
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-02-23 14:12:53 -0500

    updated sunrise/set times

--- src/java.js
+++ src/java.js
@@ -32,6 +32,8 @@ let humidityElement = document.querySelector("#now-humidity");
 let windSpeedElement = document.querySelector("#now-wind-speed");
 let dateElement = document.querySelector("#current-date");
 let iconElement = document.querySelector("#weather-icon");
+let sunriseElement = document.querySelector("#sunrise");
+let sunsetElement = document.querySelector("#sunset");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 humidityElement.innerHTML = response.data.main.humidity;
@@ -40,6 +42,8 @@ weatherDescriptionElement.innerHTML = response.data.weather[0].description;
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
+sunriseElement.innerHTML = formatDate(response.data.sys.sunrise * 1000);
+sunsetElement.innerHTML = formatDate(response.data.sys.sunset * 1000);
 }
 
 let apiKey = "ed8ab9018735ed237ff0af3c6f9509f3";
