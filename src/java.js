e65f9d4a1e5e04381148846268eaea56f864bfaf
ddepierre2 <76847005+ddepierre2@users.noreply.github.com>
2021-02-23 14:04:50 -0500

    updated weather icon

--- src/java.js
+++ src/java.js
@@ -31,12 +31,15 @@ let weatherDescriptionElement = document.querySelector("#now-weather-description
 let humidityElement = document.querySelector("#now-humidity");
 let windSpeedElement = document.querySelector("#now-wind-speed");
 let dateElement = document.querySelector("#current-date");
+let iconElement = document.querySelector("#weather-icon");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 humidityElement.innerHTML = response.data.main.humidity;
 windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
 weatherDescriptionElement.innerHTML = response.data.weather[0].description;
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "ed8ab9018735ed237ff0af3c6f9509f3";
